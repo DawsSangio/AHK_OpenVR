a234f36a9fd18a86d4813b7927dd96088462989b
Davide Sangiorgi <daws72@gmail.com>
2023-02-02 18:13:52 +0100

    wip Necksafe

--- auto_openvr/avr_dllmain.cpp
+++ auto_openvr/avr_dllmain.cpp
@@ -313,10 +313,15 @@ extern "C"
 		float angleR = yaw * M_PI / 180.0;
 		float c = std::cosf(angleR);
 		float s = std::sinf(angleR);;
-		HmdMatrix34_t newSeatedPose =
+	/*	HmdMatrix34_t newSeatedPose =
 		{ zeroSeatedpose.m[0][0]*c,  zeroSeatedpose.m[0][1], zeroSeatedpose.m[0][2]*s, zeroSeatedpose.m[0][3],
 		  zeroSeatedpose.m[1][0],    zeroSeatedpose.m[1][1], zeroSeatedpose.m[1][2],   zeroSeatedpose.m[1][3],
-		  zeroSeatedpose.m[2][0]*-s, zeroSeatedpose.m[2][1], zeroSeatedpose.m[2][2]*c, zeroSeatedpose.m[2][3]};
+		  zeroSeatedpose.m[2][0]*-s, zeroSeatedpose.m[2][1], zeroSeatedpose.m[2][2]*c, zeroSeatedpose.m[2][3]};*/
+
+		HmdMatrix34_t newSeatedPose =
+		{ zeroSeatedpose.m[0][0] * -1, zeroSeatedpose.m[0][1], zeroSeatedpose.m[0][2],      zeroSeatedpose.m[0][3],
+		  zeroSeatedpose.m[1][0],      zeroSeatedpose.m[1][1], zeroSeatedpose.m[1][2],      zeroSeatedpose.m[1][3],
+		  zeroSeatedpose.m[2][0],      zeroSeatedpose.m[2][1], zeroSeatedpose.m[2][2] * -1, zeroSeatedpose.m[2][3] };
 		
 		VRChaperoneSetup()->SetWorkingSeatedZeroPoseToRawTrackingPose(&newSeatedPose);
 		VRChaperoneSetup()->ShowWorkingSetPreview();
